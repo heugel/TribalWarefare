c87c451dbf4efeb915d0d92177a8cd7e848b1a6a
Evan Heugel <heugel@umich.edu>
2018-03-06 21:31:00 -0500

    organizing multiplayer

--- Assets/Scripts/Knockbacker.cs
+++ Assets/Scripts/Knockbacker.cs
@@ -1,9 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Networking;
 
-public class Knockbacker : NetworkBehaviour {
+public class Knockbacker : MonoBehaviour {
 
     public float knockback = 4;
     public float blockback = 1;
@@ -11,19 +10,13 @@ public class Knockbacker : NetworkBehaviour {
 
     public float damage = 0;
 
-    [Command]
-    void Cmd_Caller(GameObject go, Vector3 kbvec)
-    {
-        go.GetComponent<HitstunCaller>().Rpc_Hitstun(hitstun, kbvec);
-
-    }
-
+    public PlayerInteraction PI;
 
     private void OnTriggerEnter(Collider other)
     {
         
         Rigidbody otherrb = other.gameObject.GetComponent<Rigidbody>();
-        Vector3 kbvec = transform.GetChild(0).forward;
+        Vector3 kbvec = PI.gameObject.transform.GetChild(0).forward;
         kbvec.y = .2f;
         kbvec *= knockback;
         if (otherrb)
@@ -32,15 +25,17 @@ public class Knockbacker : NetworkBehaviour {
             //play sound effect
             //based on tag?
 
-            otherrb.AddForce(kbvec, ForceMode.VelocityChange);
             //add team checker for these parts
             if (otherrb.gameObject.tag == "Player")
             {
+                PI.Cmd_Hitstun(otherrb.gameObject, hitstun);
+                PI.Cmd_Knockback(otherrb.gameObject, kbvec);
+                PI.Cmd_Damage(otherrb.gameObject,damage);
                 //Debug.Log("network hit");
                 //otherrb.gameObject.GetComponent<HitstunCaller>().Rpc_Hitstun(hitstun);
 
                 //otherrb.gameObject.GetComponent<Health_Caveman>().Damage(damage);
-                Cmd_Caller(otherrb.gameObject, kbvec);
+                //Cmd_Caller(otherrb.gameObject, kbvec);
                 //if (otherrb.gameObject.GetComponent<Health_Caveman>().GetCurHealth() <= 0)
                 //{
                     //+1 kill
@@ -54,6 +49,10 @@ public class Knockbacker : NetworkBehaviour {
             {
                 otherrb.gameObject.GetComponent<AI_Caveman>().Hitstun(hitstun);
                 
+            }
+            else
+            {
+                otherrb.AddForce(kbvec, ForceMode.VelocityChange);
             }
         }
     }
---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class PlayerInteraction : NetworkBehaviour {
+
+
+    [ClientRpc]
+    public void Rpc_Hitstun(float time)
+    {
+        if (GetComponent<Caveman_RB>().enabled && isLocalPlayer)
+        {
+            GetComponent<Caveman_RB>().Hitstun(time);
+        }
+    }
+
+    [ClientRpc]
+    public void Rpc_Knockback(Vector3 kbvec)
+    {
+        if (GetComponent<Caveman_RB>().enabled && isLocalPlayer)
+        {
+            GetComponent<Rigidbody>().AddForce(kbvec, ForceMode.VelocityChange);
+        }
+    }
+
+    [ClientRpc]
+    public void Rpc_Damage(float dam)
+    {
+        if (GetComponent<Caveman_RB>().enabled && isLocalPlayer)
+        {
+            GetComponent<Health_Caveman>().Damage(dam);
+        }
+    }
+
+    [Command]
+    public void Cmd_Hitstun(GameObject go, float time)
+    {
+      //  if (GetComponent<Caveman_RB>().enabled && isLocalPlayer)
+      //  {
+            go.GetComponent<PlayerInteraction>().Rpc_Hitstun(time);
+      //  }
+    }
+
+    [Command]
+    public void Cmd_Knockback(GameObject go, Vector3 kbvec)
+    {
+       // if (GetComponent<Caveman_RB>().enabled && isLocalPlayer)
+       // {
+            go.GetComponent<PlayerInteraction>().Rpc_Knockback(kbvec);
+       // }
+    }
+
+    [Command]
+    public void Cmd_Damage(GameObject go, float dam)
+    {
+        //if (GetComponent<Caveman_RB>().enabled && isLocalPlayer)
+        //{
+            go.GetComponent<PlayerInteraction>().Rpc_Damage(dam);
+        //}
+    }
+
+}
